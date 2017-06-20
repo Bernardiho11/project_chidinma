71394a64f5d3a4690d3b32e5b9904d788e552aaf
Bernard O <bernardiho4personnal@gmail.com>
2017-06-20 20:50:21 +0100

    This is the folder structure for css and javascript.

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sign up your Motor vehicle </title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <ul>
+            <li>hsdchsdv</li>
+            <li>dcsdfvc</li>
+            <li>csdfvs</li>
+            <li>vsfv</li>
+        </ul>
+        <script src="js/main.js"></script>
+    </body>
+</html>
