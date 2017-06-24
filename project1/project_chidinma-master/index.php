0526bd41cebecd1b4c19e74d7d1672adc5e72956
chidsss <chinwogbo@gmail.com>
2017-06-24 20:53:05 +0100

    Add files via upload

---
+++ project1/project_chidinma-master/index.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Sign up your Motor vehicle </title>
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+    	<div class="wrapper">
+    		<header id="header" class="clearfix">
+	        	<div class="left_header">
+	        		<span class="img_holder"><img src="images/icon.png" alt="MV-reg Logo"></span>
+	        		<h4>How it works</h4>
+	        		<h4>About Mv-REG</h4>
+	        	</div>
+	        	<div class="right_header">
+	        		<ul class="clearfix">
+	        		    <li class="login_holder">
+	        		    	<input type="text" name="" value="" placeholder="Username/Email">
+	        		    </li>
+	        		    <li class="login_holder">
+	        		    	<input type="password" name="" value="" placeholder="Password">
+	        		    </li>
+		        		<li class="social_login">
+		        		   	<div class="inner_social_login">
+			        		   	<span>Login</span>
+		        		    	<div>
+		        		    		<img src="images/facebool.png" alt="Facebook SignUp Image">		
+		        		    		<img src="images/google.png" alt="Google SignUp Image">		
+		        		    	</div>
+		        		   	</div>
+	        		    </li>
+	        		    <li class="login_holder">
+	        		    	<button type="">Forgot Password?</button>
+	        		    </li>
+	        		</ul>
+	        	</div>
+	        </header><!-- /header -->
+			<div class="container">
+				<div class="form_container">
+					ddfgf
+				</div>
+			</div>
+			<footer>
+				<img src="images/SignUp-09.png" alt="">
+			</footer>
+    	</div>
+	        
+        <script src="js/main.js"></script>
+    </body>
+</html>
